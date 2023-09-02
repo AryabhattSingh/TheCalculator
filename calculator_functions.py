6ff175f0c3f090bf02915e2c15a3c26d8170dfb6
Aryabhatt <bhattsahab314@gmail.com>
2023-09-02 11:09:18 +0530

    Added the 4 basic maths operations

---
+++ calculator_functions.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    """Takes two numbers num1 and num2 as input and returns their sum"""
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Takes two numbers num1 and num2 as input and returns the value obtained when num2 is subtracted
+     from num1"""
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    """Takes two numbers num1 and num2 as input and returns their multiplication"""
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """Takes two numbers num1 and num2 as input and returns the value obtained when num1 is divided by
+    num2"""
+    return num1 / num2
